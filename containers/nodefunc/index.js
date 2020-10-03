501528a8a5d62ae969556dc1d35e4bb11aa7cff8
Alicia Barnash <a.barnash@gmail.com>
2020-10-03 19:51:01 -0400

    Add redis service and connected frontend node service

--- containers/nodefunc/index.js
+++ containers/nodefunc/index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const app = express();
 
 const redis = require('redis')
+const uuid = require('uuid')
+
+const asyncRedis = require("async-redis");
+
 /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
 const client = redis.createClient({
-  port: process.env.PORT || 6379,
-  host: process.env.HOST || 'localhost',
-  password: process.env.PASSWORD || '',
+  port: process.env.REDIS_LEADER_SERVICE_PORT || 6379,
+  host: process.env.REDIS_LEADER_SERVICE_HOST || 'localhost',
+  // password: process.env.PASSWORD || '',
+})
+
+const asyncRedisClient = asyncRedis.decorate(client);
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+app.post('/key', (req, res) => {
+  console.log('Hello world received a post request');
+
+
+  client.set("key", uuid.v4())
+
+  res.send('Success')
+})
+
+app.get('/key', async (req, res) => {
+  console.log('Hello world received a get request');
+  const value = await asyncRedisClient.get("key");
+  console.log(value);
+  res.send(value)
 })
 
 app.get('/', (req, res) => {
